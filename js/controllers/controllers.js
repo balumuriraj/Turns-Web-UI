84fb5d2751a362c7a29779e495456dd9ae30c0da
MohanRaj <balumuriraj@gmail.com>
2014-07-05 17:15:22 -0700

    pettlu

--- js/controllers/controllers.js
+++ js/controllers/controllers.js
@@ -7,6 +7,33 @@ app.controller('main-Controller', [ '$scope', function ($scope) {
 
 }]);
 
+app.controller('dashboard-Controller', [ '$scope', function ($scope) {
+
+    $scope.notifications = { 'results': [
+        {
+            'activity': 'Roomies',
+            'icon': 'rocket3',
+            'color': 'fbc73b'
+        },
+        {
+            'activity': 'Dinner',
+            'icon': 'rocket3',
+            'color': '47a3da'
+        },
+        {
+            'activity': 'Lunch',
+            'icon': 'rocket3',
+            'color': '7ea568'
+        },
+        {
+            'activity': 'Washing',
+            'icon': 'rocket3',
+            'color': 'f06060'
+        }
+
+    ]}
+}]);
+
 app.controller('activities-Controller', [ '$scope', function ($scope) {
 
     $scope.feeds = { 'results': [
