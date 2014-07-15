58ed847ab17c0cb3835d76414754612a0cb04c2f
MohanRaj <balumuriraj@gmail.com>
2014-07-14 17:48:36 -0700

    signin

--- js/controllers/controllers.js
+++ js/controllers/controllers.js
@@ -1,5 +1,6 @@
 //This controller retrieves data from the multimediaService and associates it with the $scope
 //The $scope is ultimately bound to the multimedia view
+    
 app.controller('main-Controller', [ '$scope', function ($scope) {
 
     $scope.activities = [
---
+++ js/homeapp.js
@@ -0,0 +1,7 @@
+var app = angular.module('hometurns',['ngRoute']);
+
+//This configures the routes and associates each route with a view and a controller
+app.config(function ($routeProvider) {
+    $routeProvider
+    
+});
