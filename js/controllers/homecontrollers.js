0a166a81038a78c68bf082c5c3d19745aa73e3f5
MohanRaj <balumuriraj@gmail.com>
2014-07-14 22:54:27 -0700

    signup

--- js/controllers/homecontrollers.js
+++ js/controllers/homecontrollers.js
@@ -1,6 +1,18 @@
 app.controller('login-Controller', [ '$scope', function ($scope) {
     $scope.user = {email: '', password: ''};
-    $scope.test = "asdasd";
+    // function to submit the form after all validation has occurred			
+		$scope.submitForm = function(isValid) {
+
+			// check to make sure the form is completely valid
+			if (isValid) {
+				alert('our form is amazing');
+			}
+
+		};
+}]);
+
+app.controller('signup-Controller', [ '$scope', function ($scope) {
+    $scope.user = {name: '', email: '', password: ''};
     // function to submit the form after all validation has occurred			
 		$scope.submitForm = function(isValid) {
 
